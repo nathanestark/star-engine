4d298d48f1b49e1797220699a2fab01b69c42003
Nathan E. Stark <nathan.e.stark@gmail.com>
2025-04-07 13:33:19 -0400

    Add linting/prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import { fileURLToPath } from "node:url";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+	includeIgnoreFile(fileURLToPath(new URL(".gitignore", import.meta.url))),
+  globalIgnores(["webpack.config.js"]),  
+  globalIgnores(["static/"]),
+]);
