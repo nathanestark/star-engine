2fb838e5c6d97840e333d600aaf326262bb3bc06
Nathan E. Stark <nathan.e.stark@gmail.com>
2025-04-07 10:53:49 -0400

    Update to modern stack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,13 @@
 const path = require('path');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const baseConfig = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     publicPath: '/static/',
-    libraryTarget: 'umd',
-    library: ['starengine']
   },
   plugins: [
-    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
@@ -19,59 +15,63 @@ const baseConfig = {
     }),
   ],
   module: {
-    loaders: [
+    rules: [
       {
-        test: /\.js$/,
-        loader: 'babel',
+        test: /\.m?js$/,
         exclude: /node_modules/,
-        query: {
-          presets:['es2015']
-        },
-        include: __dirname
-      },
-      {
-        test: /\.json$/,
-        loader: 'json',
-        include: __dirname
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
       }
     ]
   },
   externals: {},
   devServer: {
-    contentBase: path.join(__dirname, 'static'),
+    static: {
+      directory: path.join(__dirname, 'static'),
+    },
     historyApiFallback: true,
     host: "0.0.0.0",
-    port: "3000",
-    stats: {
-      colors: true,
-      hash: false,
-      version: false,
-      timings: false,
-      assets: true,
-      chunks: false,
-      modules: false,
-      reasons: false,
-      children: false,
-      source: false,
-      errors: true,
-      errorDetails: true,
-      warnings: true
+    port: 3000,
+    client: {
+      logging: 'info',
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+      progress: true,
+    },
+    compress: true,
+    devMiddleware: {
+      stats: {
+        colors: true,
+        hash: false,
+        version: false,
+        timings: false,
+        assets: true,
+        chunks: false,
+        modules: false,
+        reasons: false,
+        children: false,
+        source: false,
+        errors: true,
+        errorDetails: true,
+        warnings: true
+      }
     }
   }
 }
 
 const productionConfig = Object.assign({}, baseConfig, {
   entry: { 
-    "star-engine": './source/star-engine.js',
+    "n-body": './source/n-body.js', 
+    "bouncing-balls": './source/bouncing-balls.js', 
+    "asteroids": './source/asteroids.js',
   },
-  plugins: baseConfig.plugins.concat([
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      output: {
-        comments: false,
-      },
-    })
-  ]),
+  optimization: { minimize: true }
 })
 
 const configs = {
@@ -79,14 +79,8 @@ const configs = {
   production: productionConfig,
 
   development: Object.assign({}, baseConfig, {
-    plugins: baseConfig.plugins.concat([
-      new webpack.ProvidePlugin({
-            Promise: 'imports?this=>global!exports?global.Promise!es6-promise'
-      }),
-    ]),
     devtool: 'inline-source-map',
     entry: { 
-//      "star-engine": './source/star-engine.js',
       "n-body": './source/n-body.js', 
       "bouncing-balls": './source/bouncing-balls.js', 
       "asteroids": './source/asteroids.js',
